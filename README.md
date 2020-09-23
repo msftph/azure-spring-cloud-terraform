862ad612386408787ec911cbb25f06209bf16a04
msftph <msftph@users.noreply.github.com>
2020-09-23 12:20:22 -0400

    initial revision

---
+++ README.md
@@ -0,0 +1,35 @@
+# Azure Spring Cloud Terraform
+
+Creates an azure spring cloud instance with terraform
+
+## Configuration
+
+### Authentication
+
+You must setup authentication as specified in the azurerm terraform provider: 
+
+* [Authenticating](https://www.terraform.io/docs/providers/azurerm/index.html#authenticating-to-azure)
+
+### Variables
+
+Change the values in the terraform.tfvars file to the values that match your environment
+
+```tfvars
+resource_group = "spring_cloud"
+location = "East US"
+private_key_path = "private_key"
+```
+
+## Run
+
+```bash
+terraform init
+```
+
+```bash
+terraform plan
+```
+
+```bash
+terraform apply
+```
---
+++ main.tf
@@ -0,0 +1,30 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.28.0"  
+  features {}
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "default"{
+    name        = var.resource_group
+    location    = var.location
+}
+
+# Create Spring Cloud Service
+resource "azurerm_spring_cloud_service" "default" {
+  name                = "example-springcloud"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  sku_name            = "B0"
+
+  config_server_git_setting {
+    uri          = "https://github.com/msftph/azure-spring-cloud-terraform.git"
+    label        = "config"
+    search_paths = ["dir1", "dir2"]
+
+    ssh_auth{
+        private_key = file("private_key")
+    }
+  }
+}
